c662d25d7aeb43197749478b0907d7642e2662d3
Ivaneisha02 <90430746+Ivaneisha02@users.noreply.github.com>
2023-08-31 17:22:42 -0400

    Intial commit.

---
+++ LSP_Assignments_Fall_2023/src/org/howard/edu/lsp/assignment1/HelloWorld.java
@@ -0,0 +1,14 @@
+package org.howard.edu.lsp.assignment1;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		/**
+		 * Ivaneisha Lee
+		 */
+		
+		System.out.println("Hello World");
+
+	}
+
+}
