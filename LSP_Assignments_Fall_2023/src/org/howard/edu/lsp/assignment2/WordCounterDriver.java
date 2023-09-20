74765552b136195a8b255870bfdf267365df9c40
Ivaneisha02 <90430746+Ivaneisha02@users.noreply.github.com>
2023-09-19 23:12:55 -0400

    added needed classes to repo

---
+++ LSP_Assignments_Fall_2023/src/org/howard/edu/lsp/assignment2/WordCounterDriver.java
@@ -0,0 +1,15 @@
+package org.howard.edu.lsp.assignment2;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.net.URL;
+import java.util.Scanner;
+
+public class WordCounterDriver {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
