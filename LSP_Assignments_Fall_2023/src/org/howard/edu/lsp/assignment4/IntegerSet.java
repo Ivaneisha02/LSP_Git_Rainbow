ddd0cb10aba5d6ff1fc7374ab731b4c559477298
Ivaneisha02 <90430746+Ivaneisha02@users.noreply.github.com>
2023-10-15 20:53:47 -0400

    added methods to assignment 4

---
+++ LSP_Assignments_Fall_2023/src/org/howard/edu/lsp/assignment4/IntegerSet.java
@@ -0,0 +1,97 @@
+package org.howard.edu.lsp.assignment4;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class IntegerSet {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	};
+	
+	private List<Integer> set = new ArrayList<Integer>();
+	
+	//Default Constructor
+	public IntegerSet() {
+		
+	};
+	
+	//Constructor if you want to pass in already initialized ArrayList
+	public IntegerSet(ArrayList<Integer> set) {
+		this.set=set;
+	};
+	
+	//Clears the internal representation of the set
+	public void clear() {
+		
+	};
+	
+	//returns the length of the set
+	public int length() {
+		
+	};
+	
+	//returns true if 2 sets are equal
+	public boolean equals(Object o) {
+		
+	};
+	
+	//returns true if the set contains the value, otherwise false
+	public boolean contains(int value) {
+		
+	};
+	
+	//returns largest item in the set
+	public int largest() {
+		
+	};
+	
+	//returns the smallest item in the set
+	public int smallest() {
+		
+	};
+	
+	//adds an item to the set if not already there
+	public void add(int item) {
+		
+	};
+	
+	//removes an item from the set if there
+	public void remove(int item) {
+		
+	};
+	
+	//set union
+	public void union(IntegerSet intSetb) {
+		
+	};
+	
+	//set intersection
+	public void intersect(IntergetSet intSetb) {
+		
+	};
+	
+	//set difference
+	public void diff(IntgerSet intSetb) {
+		
+	};
+	
+	//set complement, all elements not in set1
+	public void complement(IntegerSet intSetb) {
+		
+	};
+	
+	//returns true if set is empty, false otherwise
+	public boolean isEmpty() {
+		
+	};
+	
+	//returns the string representation of your set
+	public String toString() {
+		
+	};
+	
+	
+
+}
