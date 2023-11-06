068b263aae1a750dddd1c5503da0ecfd255a48a4
Ivaneisha02 <90430746+Ivaneisha02@users.noreply.github.com>
2023-11-06 13:41:43 -0500

    Added test class and copied hw 4 to file

---
+++ LSP_Assignments_Fall_2023/src/org/howard/edu/assignment5/Driver.java
@@ -0,0 +1,61 @@
+package org.howard.edu.assignment5;
+
+
+public class Driver {
+
+	public static void main(String[] args) throws IntegerSetException {
+		// TODO Auto-generated method stub
+		IntegerSet set1 = new IntegerSet();
+		set1.add(1);
+		set1.add(2);
+		set1.add(3);
+		set1.add(4);
+		
+		System.out.println("Value of set1 is: " + set1.toString());
+		System.out.println("Smallest value in Set1 is: " + set1.smallest());
+		System.out.println("Largest value in Set1 is: " + set1.largest());
+		System.out.println("The length of set1 is: "+ set1.length());
+
+		IntegerSet set2 = new IntegerSet();
+		set2.add(4);
+		set2.add(5);
+		System.out.println("Value of set2 is: " + set2.toString());
+		set1.union(set2);	// union of set1 and set2
+		System.out.println("Result of union of Set1 and Set2: " + set1.toString());
+		
+		set1.clear();
+		System.out.println("Set 1 after being cleared: "+set1.toString());
+		set1.add(1);
+		set1.add(2);
+		set1.add(3);
+		set1.add(4);
+		set1.intersect(set2);
+		System.out.println("Result of intersection of Set1 and Set2: "+set1.toString());
+		set1.clear();
+		
+		System.out.println("Does set2 contain 3? "+ set2.contains(3));
+		System.out.println("Does set2 contain 5? "+ set2.contains(5));
+		
+		set1.add(4);
+		set1.add(5);
+		System.out.println("The new value of set1: "+set1.toString());
+		System.out.println("Is set1 equal to set2? "+set1.equals(set2));
+		set1.remove(5);
+		System.out.println("Set1 after removing 5: "+set1.toString());
+		set1.diff(set2);
+		set1.add(1);
+		set1.add(2);
+		set1.add(3);
+		
+		System.out.println("New value of set1 "+set1.toString());
+		System.out.println("Result of diff of set1 and set2: "+set1.toString());
+		
+		set1.complement(set2);
+		System.out.println("Result of complement of set1 and set2: "+set1.toString());
+		System.out.println("Is set2 empty? "+set2.isEmpty());
+		set2.clear();
+		System.out.println("Result of largest item in set2 after set2 is cleared: "+set2.largest());
+		
+	}
+
+}
---
+++ LSP_Assignments_Fall_2023/src/org/howard/edu/assignment5/IntegerSet.java
@@ -0,0 +1,163 @@
+package org.howard.edu.assignment5;
+
+import java.util.ArrayList;
+
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+
+public class IntegerSet {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	};
+	
+	private List<Integer> set = new ArrayList<Integer>();
+	
+	//Default Constructor
+	public IntegerSet() {
+		
+	};
+	
+	//Constructor if you want to pass in already initialized ArrayList
+	public IntegerSet(ArrayList<Integer> set) {
+		this.set=set;
+	};
+	
+	//Clears the internal representation of the set
+	public void clear() {
+		set.clear();
+	};
+	
+	//returns the length of the set
+	public int length() {
+		return set.size();
+	};
+	
+	//returns true if 2 sets are equal
+	
+	public boolean equals(Object o) {
+		
+		if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        IntegerSet that = (IntegerSet) o;
+
+        // Convert both sets to sets to eliminate duplicate elements and compare them
+        Set<Integer> set1 = new HashSet<>(this.set);
+        Set<Integer> set2  = new HashSet<>(that.set);
+
+        return set1.equals(set2);
+	};
+	
+	//returns true if the set contains the value, otherwise false
+	public boolean contains(int value) {
+		if (set.contains(value)){
+			return true;
+		};
+		return false;
+		
+	};
+	
+	//returns largest item in the set
+	public int largest() throws IntegerSetException {
+		if (set.isEmpty()) {
+            throw new IntegerSetException("Set is empty. Cannot find the largest item.");
+        }
+		return Collections.max(set);
+	};
+	
+	//returns the smallest item in the set
+	public int smallest() throws IntegerSetException {
+		if (set.isEmpty()) {
+            throw new IntegerSetException("Set is empty. Cannot find the largest item.");
+        }
+		return Collections.min(set);
+	};
+	
+	//adds an item to the set if not already there
+	public void add(int item) {
+		if (!set.contains(item)){
+			set.add(item);
+		}
+		
+	};
+	
+	//removes an item from the set if there
+	void remove(int item) {
+		
+		if (set.contains(item)) {
+			set.remove(Integer.valueOf(item));
+		}
+		
+	};
+	
+	//set union
+	public void union(IntegerSet intSetb) {
+		set.addAll(intSetb.set);
+		Set<Integer> newset = new HashSet<Integer>(set);
+	}
+	
+	//set intersection
+	public void intersect(IntegerSet intSetb) {
+		set.retainAll(intSetb.set);
+	};
+	
+	//set difference
+	public void diff(IntegerSet intSetb) {
+		set.removeAll(intSetb.set);
+	};
+	
+	//set complement, all elements not in set1
+	public void complement(IntegerSet intSetb) {
+		// Create a new ArrayList to store the complement
+        List<Integer> complement = new ArrayList<Integer>(intSetb.set);
+        complement.removeAll(set);
+        set=complement;
+	};
+	
+	//returns true if set is empty, false otherwise
+	public boolean isEmpty() {
+		if (set.isEmpty()) {
+			return true;
+		}
+		return false;
+	};
+	
+	//returns the string representation of your set
+	@Override
+	public String toString() {
+		// Create a new ArrayList 
+	    ArrayList<Integer> newList = new ArrayList<Integer>(); 
+	    // Traverse through the first list 
+	    for (Integer element : set) { 
+	    	// If this element is not present in newList 
+	        // then add it 
+	        if (!newList.contains(element)) { 
+	        	newList.add(element); 
+	        } 
+	    } 
+		return newList.toString();
+	};
+	
+
+	
+
+}
+
+class IntegerSetException extends Exception
+{
+      private static final long serialVersionUID = 1L;
+
+	// Parameterless Constructor
+      public IntegerSetException() {}
+
+      // Constructor that accepts a message
+      public IntegerSetException(String message)
+      {
+         super(message);
+      }
+ }
