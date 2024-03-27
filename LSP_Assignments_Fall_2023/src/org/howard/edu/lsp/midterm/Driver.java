15add233e1f201d8634d1cb9f1d52ad9a97ec246
Ivaneisha02 <90430746+Ivaneisha02@users.noreply.github.com>
2024-03-27 14:10:16 -0400

    question1 - 2024 midterm
    could not upload directly from eclipse

---
+++ LSP_Assignments_Fall_2023/src/org/howard/edu/lsp/midterm/Driver.java
@@ -0,0 +1,11 @@
+package org.howard.edu.lsp.midterm.question1;
+public class Driver {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(SecurityOps.encrypt("I love CSCI363" ));
+	}
+
+	
+	
+}
---
+++ LSP_Assignments_Fall_2023/src/org/howard/edu/lsp/midterm/SecurityOps.java
@@ -0,0 +1,30 @@
+package org.howard.edu.lsp.midterm.question1;
+
+public class SecurityOps {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	}
+	/**
+     * encrypting a text 
+     * characters at even indices are moved to the front of a string
+     * characters at odd indices are moved to the back of a string
+     */
+	public static String encrypt(String text) {
+		// go for it!!
+		char[] textArray = text.toCharArray();
+		String front = "";
+		String back = "";
+		for (int i = 0; i < textArray.length; i++) {
+			if (Character.isLetterOrDigit(textArray[i]) && i %2 == 0 ) {
+				front += textArray[i];
+			} else if (Character.isLetterOrDigit(textArray[i]) && i %2 == 1 ) {
+				back += textArray[i];
+			}
+		}
+		return front+back;
+	}
+
+	
+}
