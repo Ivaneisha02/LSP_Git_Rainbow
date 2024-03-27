ab3e39c64578f3df8e984cc296142ae085e738b4
Ivaneisha02 <90430746+Ivaneisha02@users.noreply.github.com>
2024-03-27 14:12:50 -0400

    question1 2024

--- LSP_Assignments_Fall_2023/src/org/howard/edu/lsp/midterm/problem1/Driver.java
+++ LSP_Assignments_Fall_2023/src/org/howard/edu/lsp/midterm/problem1/Driver.java
@@ -1,17 +1,11 @@
-package org.howard.edu.lsp.midterm.problem1;
-
+package org.howard.edu.lsp.midterm.question1;
 public class Driver {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Book book1 = new Book("To Kill a MockingBird", "Harper Lee", 1960);
-        Book book2 = new Book("To Kill a MockingBird", "Harper Lee", 1960);
-        Book book3 = new Book("1984", "George Orwell", 1960);
-        System.out.println("book 1 equals book2:" + book1.equals(book2));
-        System.out.println("book 1 equals book3:" + book1.equals(book3) +" "+ 
-       book1.toString());
-
-
+		System.out.println(SecurityOps.encrypt("I love CSCI363" ));
 	}
 
+	
+	
 }
